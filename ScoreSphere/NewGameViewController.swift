8f0981942afc192e039694fe3cd792af6d00ccab
omernyr <omer.nayir.mdbf17@iste.edu.tr>
2023-05-09 00:23:03 +0300

    Add NewGameVC

---
+++ ScoreSphere/NewGameViewController.swift
@@ -0,0 +1,18 @@
+//
+//  NewGameViewController.swift
+//  ScoreSphere
+//
+//  Created by macbook pro on 8.05.2023.
+//
+
+import UIKit
+
+class NewGameViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemGreen
+    }
+    
+}

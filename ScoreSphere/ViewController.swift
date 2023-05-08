4277cb849b16043d29f2f0ce2bb0c46a075e8908
omernyr <omer.nayir.mdbf17@iste.edu.tr>
2023-05-08 18:50:37 +0300

    Initial Commit

---
+++ ScoreSphere/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  ScoreSphere
+//
+//  Created by macbook pro on 8.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemTeal
+    }
+}

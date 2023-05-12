6c33bb4099ed5390401e3bbbfd24c4b621736ed1
omernyr <omer.nayir.mdbf17@iste.edu.tr>
2023-05-12 22:47:44 +0300

    add ColorCollectionViewCell

---
+++ ScoreSphere/ColorCollectionViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  ColorCollectionViewCell.swift
+//  ScoreSphere
+//
+//  Created by macbook pro on 11.05.2023.
+//
+
+import UIKit
+import SnapKit
+
+class ColorCollectionViewCell: UICollectionViewCell {
+    
+    static let identifier = "ColorCollectionViewCell"
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+    }
+    
+}

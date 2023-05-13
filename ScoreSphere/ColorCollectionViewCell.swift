4daa42e552de4d1833881201e8a6af686932bd6f
omernyr <omer.nayir.mdbf17@iste.edu.tr>
2023-05-14 00:15:06 +0300

    mainVc bug fixe

--- ScoreSphere/ColorCollectionViewCell.swift
+++ ScoreSphere/ColorCollectionViewCell.swift
@@ -14,7 +14,7 @@ class ColorCollectionViewCell: UICollectionViewCell {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
+    
     }
     
     required init?(coder: NSCoder) {
